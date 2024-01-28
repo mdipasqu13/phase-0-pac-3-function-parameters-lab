51e0aef8ba4560ad9ae308952401dd9a05c2f9ef
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-01-28 01:24:08 -0500

    completed parameters lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name) {
+    return (`Hi, my name is ${name}.`);
+  }
+  introduction("${name}.");
+
+  function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  
+  }
+introductionWithLanguage("${name}", "${language}");
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+  
